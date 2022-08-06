493c1e3b994f08fd3e0cc75247380c3041e2c47e
zhanglaiya <315396460@qq.com>
2022-08-06 19:02:31 +0800

    'themd'

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    title: 'royal Zhang',
+    description: 'Just playing around',
+    themeConfig: {
+        logo: '/wjk.jpg',
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Guide', link: '/guide/' },
+            { text: 'External', link: 'https://google.com'},
+        ]
+    }
+}
